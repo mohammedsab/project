795ae454551cd79ffe7cf2f92cb8bc0a51539ec1
mohammedsab <mo7ammedsab@gmail.com>
2023-03-07 15:13:19 +0200

    redeploy

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rxn6h4g81)*3gg7qvf)!)2#zq^+hgh5^*t6!3^4hheuf&^=g&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
